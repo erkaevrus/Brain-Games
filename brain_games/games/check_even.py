7d9fe8a8b2650fad8d1ff186ba315dc46e4610d9
erkaevrus <erk_08@mail.ru>
2022-07-20 15:27:50 +0300

    fix check_even according code climate recomendation

--- brain_games/games/check_even.py
+++ brain_games/games/check_even.py
@@ -2,16 +2,20 @@ from brain_games.logics.logics import logic_games
 import random
 
 
+def is_it_even(num):
+    if num % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
 def check_even(name):
     print('Answer "yes" if the number is even, otherwise answer "no".')
     i = 1
     while i < 4:
         num = random.randint(1, 10)
         question = str(num)
-        if num % 2 == 0:
-            correct_answer = 'yes'
-        else:
-            correct_answer = 'no'
+        correct_answer = is_it_even(num)
         flag = logic_games(name, question, correct_answer, i)
         if flag is False:
             break
